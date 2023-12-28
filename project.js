7e5496cfda821c80ff7483f7e1136ce20cd47acd
Saransh-Slayer <128170040+Saransh-Slayer@users.noreply.github.com>
2023-12-28 08:53:30 +0530

    project.js

--- project.js
+++ project.js
@@ -7,12 +7,12 @@ const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
 
-document.querySelector('.check').addEventListener('click', function () {
+const execution = function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
     displayMessage('No Number!');
   } else if (guess === secNumber) {
-   
+    // document.querySelector('.message').textContent = 'Correct Number!';
     displayMessage('Correct Number!');
     document.querySelector('.number').textContent = secNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -22,26 +22,44 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-   
+    //When guess is too high.
   } else if (guess !== secNumber) {
     if (score > 1) {
-     
+      //   document.querySelector('.message').textContent = guess>secNumber ? 'Too high!' : 'Too low!';
 
       displayMessage(guess > secNumber ? 'Too high!' : 'Too low!');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      
+      //   document.querySelector('.message').textContent = 'You lost the game!';
       displayMessage('You lost the game!');
       document.querySelector('.score').textContent = 0;
-    }    
+    }
+    //When guess is too low.
+    //   } else if (guess < secNumber) {
+    //     if (score > 1) {
+    //       document.querySelector('.message').textContent = ;
+    //       score--;
+    //       document.querySelector('.score').textContent = score;
+    //     } else {
+    //       document.querySelector('.message').textContent = 'You lost the game!';
+    //       document.querySelector('.score').textContent = 0;
+    //     }
+  }
+};
+
+document.querySelector('.check').addEventListener('click', execution);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    // console.log(e.key);
+    execution();
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secNumber = Math.trunc(Math.random() * 20) + 1;
- 
+  //   document.querySelector('.message').textContent = 'Start guessing...';
   displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
